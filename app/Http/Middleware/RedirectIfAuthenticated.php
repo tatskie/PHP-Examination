2e05c92be21a2e62696b6d6e801d67fb0c64f49e
allan baruiz <allan.baruiz@gmail.com>
2021-06-24 14:09:24 +0800

    Installation and Configuration for User roles

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            if (Auth::user()->hasRole('admin')) {
+                return redirect()->to('admin/dashboard');
+            } else {
+                return redirect(RouteServiceProvider::HOME);
+            }
         }
 
         return $next($request);
